388fcd498f38e66ab2fc0de5ee9f2921a530a971
SungJin-Jin <deskhi@clonet.co.kr>
2021-03-07 15:53:25 +0900

    네트워크 통신 모듈(Retrofit, Koin, Giphy 설정 정보) 추가

---
+++ app/src/main/java/giphy/android/clone/GiphyApplication.kt
@@ -0,0 +1,30 @@
+package giphy.android.clone
+
+import androidx.multidex.MultiDexApplication
+import giphy.android.clone.feature.di.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class GiphyApplication : MultiDexApplication() {
+
+    init {
+        instance = this
+    }
+
+    companion object {
+        private var instance: GiphyApplication? = null
+
+        fun context() = instance!!
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(context())
+            modules(networkModule)
+        }
+
+    }
+
+}
---
+++ app/src/main/java/giphy/android/clone/feature/di/NetworkModule.kt
@@ -0,0 +1,29 @@
+package giphy.android.clone.feature.di
+
+import giphy.android.clone.BuildConfig
+import okhttp3.OkHttpClient
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+
+val networkModule = module {
+    factory { provideOkHttpClient() }
+    single { provideRetrofit(get()) }
+}
+
+fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl(BuildConfig.API_URL)
+        .client(okHttpClient)
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build()
+}
+
+fun provideOkHttpClient(): OkHttpClient {
+    return OkHttpClient.Builder().build()
+}
