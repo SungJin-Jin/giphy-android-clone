006093ecb4f8fcf71aff0d621d8a504f5d2d3a9a
SungJin-Jin <deskhi@clonet.co.kr>
2021-03-07 16:37:09 +0900

    Pagination 데이터 클래스 추가 #3

---
+++ app/src/main/java/giphy/android/clone/base/http/Page.kt
@@ -0,0 +1,15 @@
+package giphy.android.clone.base.http
+
+import com.google.gson.annotations.SerializedName
+
+data class Page<T>(
+    var data: List<T> = emptyList(),
+    var meta: Meta = Meta(),
+    var pagination: Pagination = Pagination()
+)
+
+data class Pagination(
+    var offset: Int = 0,
+    @SerializedName("total_count") var totalCount: Int? = 0,
+    var count: Int = 0
+)
